e214be1f7b9238f91c2742a0cee0605be25f0770
Abhishek Ganjigatti <115542238+abhishek-ganjigatti@users.noreply.github.com>
2024-12-14 08:36:09 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // Replace with your actual Google Maps API key
 const CONFIG = {
-    apiKey: 'AIzaSyD6cabFRuL0Q1w3iDh9q0_HjEAqF3hUsUU',  // Replace this with your actual API key
+    apiKey: ',  // Replace  API key
     defaultLocation: { lat: 40.7128, lng: -74.0060 }, // New York City
     defaultZoom: 13,
     maxRadius: 5000, // meters
